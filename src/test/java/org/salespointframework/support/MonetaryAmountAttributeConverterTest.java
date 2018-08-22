27ce6bb05691c864946050a4c40ea7ffe07ddba5
Oliver Gierke <ogierke@pivotal.io>
2018-08-22 12:39:20 +0200

    Hacking JDK 11.
    Upgrade to a Lombok version that's compatible with JDK > 8.
    
    Previously blocked by: https://github.com/JavaMoney/jsr354-ri/issues/208. Will be resolved with Moneta 1.4.

--- src/test/java/org/salespointframework/support/MonetaryAmountAttributeConverterTest.java
+++ src/test/java/org/salespointframework/support/MonetaryAmountAttributeConverterTest.java
@@ -19,10 +19,14 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.junit.MatcherAssert.*;
 
 import java.math.BigDecimal;
+import java.util.Locale;
+
+import javax.money.MonetaryAmount;
+import javax.money.format.MonetaryAmountFormat;
+import javax.money.format.MonetaryFormats;
 
 import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.Test;
-import org.salespointframework.support.MonetaryAmountAttributeConverter;
 
 /**
  * Unit tests for {@link MonetaryAmountAttributeConverter}
@@ -66,7 +70,21 @@ class MonetaryAmountAttributeConverterTest {
 
 	@Test // #156
 	void deserializesFormattedValues() {
-		assertThat(converter.convertToEntityAttribute("EUR 123,456.78"), is(Money.of(123456.78, "EUR")));
+
+		MonetaryAmountFormat format = MonetaryFormats.getAmountFormat(Locale.ROOT);
+
+		MonetaryAmount source = Money.of(123456.78, "EUR");
+		String formatted = format.format(source);
+
+		// TODO - Remove before merge
+
+		System.out.println(formatted); // "EUR 123,456.78", space is a 32 on JDK 8, a 160 on JDK 9 and 10
+		System.out.println((int) formatted.toCharArray()[3]); // JDK 8: 32, JDK 9: JDK 10:
+
+		MonetaryAmount foo = format.parse(formatted);
+		MonetaryAmount result = format.parse("EUR 123,456.78"); // Space is char of 32 (standard space)
+
+		assertThat(converter.convertToEntityAttribute(formatted), is(Money.of(123456.78, "EUR")));
 	}
 
 	@Test // #156
